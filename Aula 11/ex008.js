c3b5cba954921afe610fcc3681cf5a144e01c78a
RaphaelPaixao1984 <106095167+RaphaelPaixao1984@users.noreply.github.com>
2023-04-11 20:36:51 -0300

    Initial commit

---
+++ Aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){ //CONDIÇÃO SIMPLES
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança!`)
+
